765820c52d85c6da4238fb84c6fe2bb039692b60
Johan Peltenburg <j.w.peltenburg@tudelft.nl>
2020-12-10 10:55:05 +0100

    Initial attempt to implement multi buffered client

--- include/illex/raw_client.h
+++ include/illex/raw_client.h
@@ -16,6 +16,8 @@
 
 #include <string>
 #include <utility>
+#include <mutex>
+#include <memory>
 
 #include <kissnet.hpp>
 #include <putong/timer.h>
@@ -28,34 +30,41 @@
 
 namespace illex {
 
-/// A streaming client using the Raw protocol.
 struct RawClient {
+  virtual auto ReceiveJSONs(LatencyTracker *lat_tracker) -> Status = 0;
+  virtual auto Close() -> Status = 0;
+};
+
+/// A streaming client using the Raw protocol that queues received JSONs.
+struct RawQueueingClient : public RawClient {
  public:
   /**
-   * \brief Construct a new raw client.
-   * \param[in] protocol The protocol options.
-   * \param[in] host The hostname to connect to.
-   * \param[in] seq Starting sequence number.
-   * \param[out] out The raw client that will be populated by this function.
+   * \brief Construct a new queueing client.
+   * \param[in] protocol    The protocol options.
+   * \param[in] host        The hostname to connect to.
+   * \param[in] seq         Starting sequence number.
+   * \param[in] queue           The queue to put the JSONs in.
+   * \param[out] out        The raw client that will be populated by this function.
    * \return Status::OK() if successful, some error otherwise.
    */
   static auto Create(RawProtocol protocol,
                      std::string host,
                      uint64_t seq,
-                     RawClient *out) -> Status;
+                     JSONQueue *queue,
+                     RawQueueingClient *out,
+                     size_t buffer_size = ILLEX_TCP_BUFFER_SIZE) -> Status;
 
   /**
-   * \brief Receive JSONs on this raw stream client and put them in queue.
-   * \param queue           The queue to put the JSONs in.
+   * \brief Receive JSONs on this raw stream client and put them in a queue.
    * \return Status::OK() if successful, some error otherwise.
    */
-  auto ReceiveJSONs(JSONQueue *queue, LatencyTracker *lat_tracker) -> Status;
+  auto ReceiveJSONs(LatencyTracker *lat_tracker) -> Status override;
 
   /**
    * \brief Close this raw client.
    * \return Status::OK() if successful, some error otherwise.
    */
-  auto Close() -> Status;
+  auto Close() -> Status override;
 
   /// \brief Return the number of received JSONs
   [[nodiscard]] auto received() const -> size_t { return received_; }
@@ -63,6 +72,8 @@ struct RawClient {
   /// \brief Return the number of received bytes
   [[nodiscard]] auto bytes_received() const -> size_t { return bytes_received_; }
 
+  RawQueueingClient() = delete;
+  ~RawQueueingClient();
  private:
   /// The next available sequence number.
   uint64_t seq = 0;
@@ -76,7 +87,122 @@ struct RawClient {
   illex::RawProtocol protocol;
   /// The TCP socket.
   std::shared_ptr<RawSocket> client;
+  // TCP receive buffer.
+  std::byte *buffer;
+  // TCP receive buffer size.
+  size_t buffer_size;
+  // Whether the client must be closed.
+  bool must_be_closed = false;
+  // The queue to dump JSONs in.
+  JSONQueue *queue;
+};
 
+/// A structure to manage multi-buffered client implementation.
+class RawJSONBuffer {
+ public:
+  /**
+   * \brief Create a new buffer wrapper to dump JSONs in.
+   * \param[in]  buffer      The buffer address, managed elsewhere.
+   * \param[in]  capacity    The capacity of the buffer.
+   * \param[out] out         The resulting
+   * \return Status::OK() if successful, some error otherwise.
+   */
+  static auto Create(std::byte *buffer, size_t capacity, RawJSONBuffer *out) -> Status;
+
+  /// \brief Return a pointer to mutate the buffer contents.
+  auto mutable_data() -> std::byte * { return buffer_; }
+
+  /// \brief Return a pointer to read the buffer.
+  [[nodiscard]] auto data() const -> const std::byte * { return buffer_; }
+
+  /// \brief Return the allocated capacity of the buffer.
+  [[nodiscard]] auto capacity() const -> size_t { return capacity_; }
+
+  /// \brief Return the number of valid bytes in the buffer.
+  [[nodiscard]] auto size() const -> size_t { return size_; }
+
+  /// \brief Return whether the buffer is empty.
+  [[nodiscard]] auto empty() const -> bool { return size_ == 0; }
+
+  /// \brief Modify the number of valid of bytes in the buffer without bounds checking.
+  inline void SetSizeUnsafe(size_t size) { size_ = size; }
+
+  /// \brief Modify the number of valid bytes in the buffer with bounds checking.
+  auto SetSize(size_t size) -> Status;
+
+  /// \brief Set the JSON sequence number inclusive range contained in this buffer.
+  auto SetSequenceNumbers(std::pair<Seq, Seq> seq_nos) -> Status;
+
+  /// \brief Reset the buffer.
+  void Reset() { this->size_ = 0; }
+ protected:
+  RawJSONBuffer(std::byte *buffer, size_t capacity)
+      : buffer_(buffer), capacity_(capacity) {}
+  /// A pointer to the buffer.
+  std::byte *buffer_;
+  /// The number of valid bytes in the buffer.
+  size_t size_ = 0;
+  /// The capacity of the buffer.
+  size_t capacity_ = 0;
+  /// The JSONs sequence numbers contained within the buffer.
+  std::pair<Seq, Seq> seq_first_last;
+};
+
+/// A streaming client using the Raw protocol that receives the JSONs over multiple
+/// buffers with mutexes. It applies a non-blocking round-robin approach to finding an
+/// available buffer to dump the contents in, but keeps track of sequence numbers to not
+/// lose order.
+struct RawMultiBufferClient : public RawClient {
+ public:
+  /**
+   * \brief Construct a RawMultiBufferClient
+   * \param[in]  protocol The protocol options.
+   * \param[in]  host The hostname.
+   * \param[in]  seq The sequence number to start at.
+   * \param[in]  buffers The pre-allocated buffers.
+   * \param[out] out The RawMultiBufferClient output.
+   * \return Status::OK() if successful, some error otherwise.
+   */
+  static auto Create(RawProtocol protocol,
+                     std::string host,
+                     uint64_t seq,
+                     const std::vector<RawJSONBuffer *> &buffers,
+                     const std::vector<std::mutex *> &mutexes,
+                     RawMultiBufferClient *out) -> Status;
+
+  /**
+   * \brief Receive JSONs into the pre-allocated buffers.
+   *
+   * This function is meant to run in a main thread, where consuming threads access the
+   * buffers through buffer() and mutex() to absorb the data. Consuming threads must
+   * Reset() the buffers after they are done processing the data.
+   *
+   * \param lat_tracker Latency tracking device.
+   * \return Status::OK() if successful, some error otherwise.
+   */
+  auto ReceiveJSONs(LatencyTracker *lat_tracker) -> Status override;
+  auto Close() -> Status override;
+ private:
+  /// The mutexes to manage buffer access.
+  std::vector<std::mutex *> mutexes;
+  /// The buffers.
+  std::vector<RawJSONBuffer *> buffers;
+  /// The next available sequence number.
+  uint64_t seq = 0;
+  /// The number of received JSONs.
+  size_t jsons_received_ = 0;
+  /// The number of received bytes.
+  size_t total_bytes_received_ = 0;
+  /// The host name to connect to.
+  std::string host = "localhost";
+  /// The protocol options.
+  illex::RawProtocol protocol;
+  /// The TCP socket.
+  std::shared_ptr<RawSocket> client;
+  // Whether the client must be closed.
+  bool must_be_closed = false;
+  // The current buffer to receive the TCP data in.
+  size_t buffer_idx = 0;
 };
 
 }
--- src/illex/raw_client.cpp
+++ src/illex/raw_client.cpp
@@ -15,6 +15,7 @@
 #include <chrono>
 #include <memory>
 #include <iostream>
+#include <utility>
 
 #include "illex/latency.h"
 #include "illex/raw_client.h"
@@ -22,15 +23,24 @@
 
 namespace illex {
 
-using TCPBuffer = std::array<std::byte, ILLEX_TCP_BUFFER_SIZE>;
+auto RawQueueingClient::Create(RawProtocol protocol,
+                               std::string host,
+                               uint64_t seq,
+                               JSONQueue *queue,
+                               RawQueueingClient *out,
+                               size_t buffer_size) -> Status {
+  auto *buffer = static_cast<std::byte *>(malloc(buffer_size));
 
-auto RawClient::Create(RawProtocol protocol,
-                       std::string host,
-                       uint64_t seq,
-                       RawClient *out) -> Status {
+  if (buffer == nullptr) {
+    return Status(Error::RawError, "Could not allocate TCP recv buffer.");
+  }
+
   out->seq = seq;
   out->host = std::move(host);
   out->protocol = protocol;
+  out->buffer = buffer;
+  out->buffer_size = buffer_size;
+  out->queue = queue;
 
   auto endpoint = out->host + ":" + std::to_string(out->protocol.port);
   out->client = std::make_shared<RawSocket>(kissnet::endpoint(endpoint));
@@ -40,6 +50,8 @@ auto RawClient::Create(RawProtocol protocol,
   if (!success) {
     return Status(Error::RawError, "Unable to connect to server.");
   }
+
+  out->must_be_closed = true;
 
   return Status::OK();
 }
@@ -116,17 +128,15 @@ static auto EnqueueAllJSONsInBuffer(std::string *json_buffer,
   return queued;
 }
 
-auto RawClient::ReceiveJSONs(JSONQueue *queue, LatencyTracker *lat_tracker) -> Status {
+auto RawQueueingClient::ReceiveJSONs(LatencyTracker *lat_tracker) -> Status {
   // Buffer to store the JSON string, is reused to prevent allocations.
   std::string json_string;
-  // TCP receive buffer.
-  auto *recv_buffer = static_cast<std::byte *>(malloc(ILLEX_TCP_BUFFER_SIZE));
 
   // Loop while the socket is still valid.
   while (client->is_valid()) {
     try {
       // Attempt to receive some bytes.
-      auto recv_status = client->recv(recv_buffer, ILLEX_TCP_BUFFER_SIZE);
+      auto recv_status = client->recv(buffer, buffer_size);
       auto receive_time = Timer::now();
 
       // Perhaps the server disconnected because it's done sending JSONs, check the
@@ -144,7 +154,7 @@ auto RawClient::ReceiveJSONs(JSONQueue *queue, LatencyTracker *lat_tracker) -> S
       this->bytes_received_ += bytes_received;
       // We must now handle the received bytes in the TCP buffer.
       auto num_enqueued = EnqueueAllJSONsInBuffer(&json_string,
-                                                  recv_buffer,
+                                                  buffer,
                                                   bytes_received,
                                                   queue,
                                                   &this->seq,
@@ -157,14 +167,180 @@ auto RawClient::ReceiveJSONs(JSONQueue *queue, LatencyTracker *lat_tracker) -> S
     }
   }
 
-  free(recv_buffer);
+  return Status::OK();
+}
+
+auto RawQueueingClient::Close() -> Status {
+  if (must_be_closed) {
+    client->close();
+    must_be_closed = false;
+  } else {
+    return Status(Error::RawError, "Client was already closed.");
+  }
+  return Status::OK();
+}
+
+RawQueueingClient::~RawQueueingClient() {
+  if (must_be_closed) {
+    this->Close();
+  }
+  free(buffer);
+}
+
+auto RawJSONBuffer::Create(std::byte *buffer,
+                           size_t capacity,
+                           RawJSONBuffer *out) -> Status {
+  if (buffer == nullptr) {
+    return Status(Error::RawError, "Pre-allocated buffer cannot be nullptr.");
+  }
+  if (capacity == 0) {
+    return Status(Error::RawError, "Size cannot be 0.");
+  }
+  *out = RawJSONBuffer(buffer, capacity);
 
   return Status::OK();
 }
 
-auto RawClient::Close() -> Status {
-  SPDLOG_DEBUG("Closing client.");
-  client->close();
+Status RawJSONBuffer::SetSize(size_t size) {
+  if (size < capacity_) {
+    size_ = size;
+    return Status::OK();
+  } else {
+    return Status(Error::RawError,
+                  "Cannot set buffer size larger than allocated capacity.");
+  }
+}
+
+auto RawJSONBuffer::SetSequenceNumbers(std::pair<Seq, Seq> seq_nos) -> Status {
+  seq_first_last = seq_nos;
+  return Status::OK();
+}
+
+auto RawMultiBufferClient::Create(RawProtocol protocol,
+                                  std::string host,
+                                  uint64_t seq,
+                                  const std::vector<RawJSONBuffer *> &buffers,
+                                  const std::vector<std::mutex *> &mutexes,
+                                  RawMultiBufferClient *out) -> Status {
+  out->mutexes = mutexes;
+  out->buffers = buffers;
+  // Set other configuration params.
+  out->seq = seq;
+  out->host = std::move(host);
+  out->protocol = protocol;
+  // Create an endpoint.
+  auto endpoint = out->host + ":" + std::to_string(out->protocol.port);
+  out->client = std::make_shared<RawSocket>(kissnet::endpoint(endpoint));
+  // Attempt to connect.
+  SPDLOG_DEBUG("Client connecting to {}...", endpoint);
+  auto success = out->client->connect();
+  if (!success) {
+    return Status(Error::RawError, "Unable to connect to server.");
+  }
+  // Connect successful, remember we have to close it on deconstruction.
+  out->must_be_closed = true;
+
+  return Status::OK();
+}
+
+/**
+ * \brief Count the JSONs in the buffer, so we can assign sequence numbers.
+ * \param buffer
+ * \param tcp_valid_bytes
+ * \param seq
+ * \param receive_time
+ * \param tracker
+ * \return
+ */
+static auto CountJSONsInBuffer(RawJSONBuffer *buffer,
+                               uint64_t *seq,
+                               TimePoint receive_time,
+                               LatencyTracker *tracker = nullptr)
+-> std::pair<Seq, Seq> {
+  size_t first = *seq;
+  // TODO(johanpel): implement mechanism to allow newlines within JSON objects,
+  //   this only works for non-pretty printed JSONs now.
+
+  // Scan the buffer for a newline.
+  const auto *recv_chars = reinterpret_cast<const char *>(buffer->data());
+  const char *json_start = recv_chars;
+  const char *json_end = recv_chars + buffer->size();
+  size_t remaining = buffer->size();
+  do {
+    json_end = std::strchr(json_start, '\n');
+    if (json_end == nullptr) {
+      remaining = 0;
+    } else {
+      // Place the receive time for this JSON in the tracker.
+      if (tracker != nullptr) { tracker->Put(*seq, 0, receive_time); }
+      (*seq)++;
+      // Move the start scan index to the character after the newline.
+      json_start = json_end + 1;
+      // Calculate the remaining number of bytes in the buffer.
+      remaining = (recv_chars + buffer->size()) - json_start;
+    }
+    // Repeat until there are no remaining bytes.
+  } while (remaining > 0);
+
+  return {first, (*seq) - 1};
+}
+
+auto RawMultiBufferClient::ReceiveJSONs(LatencyTracker *lat_tracker) -> Status {
+  // Loop while the socket is still valid.
+  while (client->is_valid()) {
+    try {
+      bool empty = true;
+      RawJSONBuffer *buf = nullptr;
+      // Find an empty buffer mutex to lock.
+      while (!empty || !mutexes[buffer_idx]->try_lock()) {
+        buf = buffers[buffer_idx];
+        // Check if the buffer still has some data for a downstream thread to process.
+        if (!buf->empty()) {
+          mutexes[buffer_idx]->unlock();
+          empty = false;
+        }
+        buffer_idx++;
+      }
+      // We were able to lock a buffer mutex.
+
+      // Attempt to receive some bytes.
+      auto recv_status = client->recv(buf->mutable_data(), buf->size());
+      auto receive_time = Timer::now();
+
+      // Perhaps the server disconnected because it's done sending JSONs, check the
+      // status.
+      auto bytes_received = std::get<0>(recv_status);
+      auto sock_status = std::get<1>(recv_status).get_value();
+      if (sock_status == kissnet::socket_status::cleanly_disconnected) {
+        SPDLOG_DEBUG("Server has cleanly disconnected.");
+        return Status::OK();
+      } else if (sock_status != kissnet::socket_status::valid) {
+        // Otherwise, if it's not valid, there is something wrong.
+        return Status(Error::RawError,
+                      "Server error. Status: " + std::to_string(sock_status));
+      }
+      this->total_bytes_received_ += bytes_received;
+      ILLEX_ROE(buf->SetSize(bytes_received));
+      // We must now handle the received bytes in the TCP buffer.
+      auto num_enqueued = CountJSONsInBuffer(buf, &this->seq, receive_time, lat_tracker);
+      ILLEX_ROE(buf->SetSequenceNumbers(num_enqueued))
+      this->jsons_received_ += (num_enqueued.second - num_enqueued.first + 1);
+    } catch (const std::exception &e) {
+      // But first we catch any exceptions.
+      return Status(Error::RawError, e.what());
+    }
+  }
+
+  return Status::OK();
+}
+
+auto RawMultiBufferClient::Close() -> Status {
+  if (must_be_closed) {
+    client->close();
+    must_be_closed = false;
+  } else {
+    return Status(Error::RawError, "Client was already closed.");
+  }
   return Status::OK();
 }
 
